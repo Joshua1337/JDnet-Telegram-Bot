58dc3c87eb5071e74dc9471a183484c972cc4e35
Joshua Dehler <git@j0sh.de>
2016-05-29 18:01:53 +0200

    added files

---
+++ index.js
@@ -0,0 +1,37 @@
+import dude from 'debug-dude'
+const { /*debug,*/ log, info /*, warn, error*/ } = dude('bot')
+
+import { version } from '../package.json'
+info(`jdnet bot v${version} starting`)
+
+import config from '../config.json'
+
+import { connect, message } from 'coffea'
+const networks = connect(config)
+
+import fs from 'fs'
+import path from 'path'
+
+networks.on('command', (evt, reply) => {
+  log('Received command event: %o', evt)
+
+  switch (evt.cmd) {
+    case 'say':
+      reply(message(evt.channel, evt.args.join('')))
+      break;
+  }
+})
+
+networks.on('message', (evt, reply) => {
+  log('Received message event: %o', evt)
+
+  let hashtags = evt.text.match(/#([a-zA-Z]+)/)
+  hashtags && hashtags.length > 1
+
+  reply({
+    type: 'sendVoice',
+    action: 'record_audio',
+    id: evt.channel,
+    voice: fs.createReadStream(path.join(__dirname, `voice/${hashtags[1]}.mp3`))
+  })
+})
